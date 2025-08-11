29744aee866541cd72a3bc0b35730e57875be979
Farah-Stu <selma.salam45@gmail.com>
2025-08-11 19:57:26 +0100

    final update of js file

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let currentEpisodes = [];
+
 function setup() {
   const rootElem = document.getElementById("root");
   const countElem = document.getElementById("episodeCount");
@@ -5,10 +7,18 @@ function setup() {
   const showSelect = document.getElementById("showSelect");
   const input = document.getElementById("q");
 
+  input.addEventListener("input", function(){
+    const searchTerm = input.value.toLowerCase();
+    const filteredEpisodes= currentEpisodes.filter(episode => {
+    return episode.name.toLowerCase().includes (searchTerm) ||
+        episode.summary.toLowerCase().includes (searchTerm)
+    });
+
+ makePageForEpisodes(filteredEpisodes, countElem, episodeSelect);
+  });
   // Cache variables - so we never fetch same URL twice
   let allShows = null;
   let episodeCache = {};
-  let currentEpisodes = [];
 
   loadAllShows(showSelect, allShows).then(() => {
     showSelect.addEventListener("change", function () {
@@ -26,6 +36,7 @@ function setup() {
             return response.json();
           })
           .then((episodes) => {
+            currentEpisodes = episodes;
             makePageForEpisodes(episodes, countElem, episodeSelect);
           });
       }
@@ -34,8 +45,12 @@ function setup() {
 }
 
 function formatEpisodeCode(episode) {
-  return "S" + String(episode.season).padStart(2, "0") +
-         "E" + String(episode.number).padStart(2, "0");
+  return (
+    "S" +
+    String(episode.season).padStart(2, "0") +
+    "E" +
+    String(episode.number).padStart(2, "0")
+  );
 }
 
 function makePageForEpisodes(episodesList, countElem, episodeSelect) {
@@ -62,9 +77,10 @@ function makePageForEpisodes(episodesList, countElem, episodeSelect) {
     section.appendChild(title);
 
     const image = document.createElement("img");
-    let imgUrl = (episode.image && episode.image.medium)
-      ? episode.image.medium
-      : "https://via.placeholder.com/210x295?text=No+Image";
+    let imgUrl =
+      episode.image && episode.image.medium
+        ? episode.image.medium
+        : "https://via.placeholder.com/210x295?text=No+Image";
     imgUrl = imgUrl.replace(/^http:/, "https:"); // force HTTPS
     image.src = imgUrl;
     image.alt = episode.name;
